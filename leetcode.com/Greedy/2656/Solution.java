bb036dd309d16bf186677206631647f64892d30f
abdullin.af <abdullin.af@alidi.ru>
2025-01-30 20:07:10 +0300

    2656 from leetcode

---
+++ leetcode.com/Greedy/2656/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maximizeSum(int[] nums, int k) {
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > max) max = nums[i];
+        }
+        int sum = max;
+        for (int i = max + 1; i < (max + k); i++) sum += i;
+        return sum;
+    }
+}
