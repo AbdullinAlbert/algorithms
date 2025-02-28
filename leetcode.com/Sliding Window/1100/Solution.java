27ba8153ae19d400b10d0d4f1d1d9ddfd58bb4a2
abdullin.af <abdullin.af@alidi.ru>
2025-02-28 20:52:11 +0300

    1100 from leetcode

--- leetcode.com/Sliding Window/1100/Solution.java
+++ leetcode.com/Sliding Window/1100/Solution.java
@@ -2,18 +2,28 @@ class Solution {
     public int numKLenSubstrNoRepeats(String s, int k) {
         int res = 0;
         if (s.length() < k) return res;
-        for (int i = 0; i <= s.length() - k; i++) {
-            char[] chars = new char[26];
-            boolean unique = true;
-            for (int j = i; j < i + k; j++) {
-                if (chars[s.charAt(j) % 26] == 0) {
-                    chars[s.charAt(j) % 26] = s.charAt(j);
-                } else {
-                    unique = false;
-                    break;
-                }
-            }
-            if (unique) res++;
+        int size = 26;
+        char[] charsCount = new char[size];
+        int uniqueCount = 0;
+        int index = 0;
+        for (int i = 0; i < k; i++) {
+            index = s.charAt(i) % size;
+            charsCount[index]++;
+            if (charsCount[index] == 1) uniqueCount++; 
+        }
+        if (uniqueCount == k) res++;
+        int start = 1;
+        int end = k;
+        while (end < s.length()) {
+            index = s.charAt(start - 1) % size;
+            charsCount[index]--;
+            if (charsCount[index] == 0 && uniqueCount > 1) uniqueCount--;
+            index = s.charAt(end) % size;
+            charsCount[index]++;
+            if (charsCount[index] == 1 && uniqueCount < k) uniqueCount++;
+            if (uniqueCount == k) res++;
+            start++;
+            end++;
         }
         return res;
     }
